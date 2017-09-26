52d2dcab2471c9ad81fa37953df67b02627fe699
harshilvasani <vasani.h@husky.neu.edu>
2017-09-26 13:34:09 -0400

    added model and view model variable for detail

--- public/client/views/qna/detail.controller.js
+++ public/client/views/qna/detail.controller.js
@@ -6,40 +6,43 @@
         .module("MiniQuoraApp")
         .controller("DetailController", DetailController); // name of controller, function to be call
 
-    function DetailController($scope,$location, $routeParams) {
-        $scope.longText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        $scope.question = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        $scope.questionId = $routeParams.questionId
-        $scope.location = $location.url();
+    function DetailController($location, $routeParams) {
+        var vm = this;
+
+        vm.longText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        vm.Question = {}
+        vm.Question.questionText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        vm.Question.questionId = $routeParams.questionId
+        vm.location = $location.url();
         alert( "In Detail Controller for " + $routeParams.questionId);
 
-        $scope.addNewAnswer = AddNewAnswer
+        vm.addNewAnswer = AddNewAnswer
 
         function AddNewAnswer(newAnswer, questionId) {
             alert(newAnswer + ' ' +questionId)
         }
 
-        $scope.removeQuestion = RemoveQuestion
+        vm.removeQuestion = RemoveQuestion
 
         function RemoveQuestion(questionId) {
             alert("remove Question id: " + questionId)
         }
 
-        $scope.updateQuestion = UpdateQuestion
+        vm.updateQuestion = UpdateQuestion
 
         function UpdateQuestion(questionId) {
-            alert("edited Question id: " + questionId + " \nupdated Question : " + $scope.question)
+            alert("edited Question id: " + questionId + " \nupdated Question : " + vm.Question.questionText)
         }
 
-        $scope.allVote = [0,0,0,0,0]
+        vm.allVote = [0,0,0,0,0]
 
-        $scope.addVote = AddVote
-        $scope.hasVoted = [false, false, false, false, false]
+        vm.addVote = AddVote
+        vm.hasVoted = [false, false, false, false, false]
 
         function AddVote(id, value) {
-            if(!$scope.hasVoted[id]){
-                $scope.allVote[id] += value
-                $scope.hasVoted[id] = true
+            if(!vm.hasVoted[id]){
+                vm.allVote[id] += value
+                vm.hasVoted[id] = true
             }
         }
 
