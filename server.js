c9e245f9de1d4aa3550e253d0895acc09965881b
harshilvasani <harshilvasani03@gmail.com>
2017-09-27 16:09:17 -0400

    added server to host angular

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
 
-require("./public/client/app.js")(app);
+app.use(express.static(__dirname + '/public'));// configuring express to host static content present in public directory
+
+// app.get('/', function (req, res) {
+//     res.send('hello world')
+// })
 app.listen(3000);
